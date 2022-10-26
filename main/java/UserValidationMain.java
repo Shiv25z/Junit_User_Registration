bb778e49aa635ce580c2f09625c66f0b664a972d
Shiv25z <shivamzagade25899@gmail.com>
2022-10-26 18:04:11 +0530

    Welcome to Junit User Registration Problem!

---
+++ main/java/UserValidationMain.java
@@ -0,0 +1,5 @@
+public class UserValidationMain {
+    public static void main(String[] args) {
+        System.out.println("_________________ Welcome to Junit User Registration Problem! _________________");
+    }
+}
